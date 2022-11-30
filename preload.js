dd9699cb929f2daa4a6165331131b584b39d60b7
huliawsl <3150399699@qq.com>
2022-11-30 11:19:57 +0800

    添加文件，对应RTM JS MAKER 1.1.0

---
+++ preload.js
@@ -0,0 +1,4 @@
+window.ipcRenderer = require('electron').ipcRenderer;
+window.remote = require('electron').remote;
+window.fs = require("fs");
+window.JSONbig = require('json-bigint')({ "storeAsString": true });
